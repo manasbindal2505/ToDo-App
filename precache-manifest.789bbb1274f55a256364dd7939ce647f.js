46b90ac93fc3d9993f7b42e0d334ac96b7d40cde
unknown <manasbindal05@gmail.com>
2020-05-28 19:17:46 +0530

    Updates

---
+++ precache-manifest.789bbb1274f55a256364dd7939ce647f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979a7a10ece391c30c6d692eea3c0cfc",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "77d33c12653714b6ff28",
+    "url": "/Todo-app/static/css/main.34ee50ee.chunk.css"
+  },
+  {
+    "revision": "a10a0926bd0205023734",
+    "url": "/Todo-app/static/js/2.604a7ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.604a7ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d33c12653714b6ff28",
+    "url": "/Todo-app/static/js/main.60f20a8d.chunk.js"
+  },
+  {
+    "revision": "345219577da854b2edb4",
+    "url": "/Todo-app/static/js/runtime-main.d1337565.js"
+  }
+]);
